2952af71089bbc9dce551218e362cd901cc15a2e
dave <d@sofit.pw>
2023-11-24 10:10:07 +0300

    feature add servicesObject users

---
+++ bot/serviceObjects/users/__init__.py
@@ -0,0 +1,45 @@
+from datetime import date
+from pydantic import BaseModel
+
+from bot.serviceObjects.utils import UuidBase, QueryParamFields, SchemaDB
+
+
+class UserBase(BaseModel):
+    phone: str
+    firstName: str | None = None
+    lastName: str | None = None
+    secondName: str | None = None
+    nameEnglish: str | None = None
+    birthday: date | None = None
+    city: str | None = None
+
+
+class UserIn(UserBase):
+    pass
+
+
+class UserOut(UserBase, UuidBase):
+    class Config:
+        from_attributes = True
+
+
+class UserQueryParamFields(QueryParamFields):
+    pass
+
+
+class UserData(BaseModel):
+    phone: str
+    firstName: str
+    lastName: str
+    secondName: str
+    nameEnglish: str
+    city: str
+
+
+class UserDataT(BaseModel):
+    pass
+
+
+class UserSchemaDB(SchemaDB):
+    data: UserData
+    dataT: UserDataT
---
+++ bot/serviceObjects/users/manager.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from bot.models import User as UserDB
+from bot.serviceObjects.users import UserIn, UserOut, UserSchemaDB, UserQueryParamFields
+from bot.serviceObjects.utils import ServiceObjectBase, QueryParamFields
+
+
+class UserMng(ServiceObjectBase):
+    ClsDB = UserDB
+    Cls = UserOut
+    SchemaDB = UserSchemaDB
+
+    async def create(self, obj: UserIn = None, notCommit: bool = False) -> UserOut:
+        return await super().create(obj=obj, notCommit=notCommit)
+
+    async def get(self, objectDB=None, queryParams: QueryParamFields = None, **kwargs) -> UserOut:
+        return await super().get(objectDB=objectDB, queryParams=queryParams, **kwargs)
+
+    async def getList(self, queryParams: UserQueryParamFields = None, order_by: dict | None = None, **kwargs) -> List[UserOut]:
+        return await super().getList(queryParams=queryParams, query=None, order_by=order_by, **kwargs)
