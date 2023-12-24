9d7c2d63a5f5030fa32b5f52684202858f2d11b9
dave <d@sofit.pw>
2023-12-24 15:52:41 +0300

    refactor (models) add relationship

--- bot/models/coursesList.py
+++ bot/models/coursesList.py
@@ -1,12 +1,29 @@
-from sqlalchemy import Column, Index, ForeignKey
+from sqlalchemy import Index, cast, func
+from sqlalchemy.dialects.postgresql import UUID, JSONB
+from sqlalchemy.future import select
+from sqlalchemy.orm import relationship, foreign
 from sqlalchemy_utils import UUIDType
 
 from bot.models.utils import ListTableWithArchive
 
+from bot.models.employeesList import Employee
+from bot.models.productsList import Product
+
 
 class Course(ListTableWithArchive):
     __tablename__ = 'coursesList'
-    productUid = Column(UUIDType(), ForeignKey('productsList.uid'), nullable=False)
+    product = relationship("Product", lazy='joined', viewonly=True,
+                           primaryjoin=lambda: Product.uid == foreign(
+                               Course.data.cast(JSONB)["productUid"].astext.cast(UUIDType)))
 
+
+employees = select(
+    cast(func.jsonb_array_elements_text(Course.data.op('->')('employeeUids')), UUID).label("employeeUid"),
+    Course.uid.label("courseUid")).alias()
+
+# noinspection PyTypeChecker
+Course.employees = relationship(Employee, lazy='joined', viewonly=True, secondary=employees,
+                                primaryjoin=employees.c.courseUid == Course.uid,
+                                secondaryjoin=employees.c.employeeUid == Employee.uid)
 
 Index('ind_course_data_gin', Course.data, postgresql_using="gin")
