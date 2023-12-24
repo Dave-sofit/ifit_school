5e1e3f1b9c4357ee7151c1fcac547e0301d56ce4
dave <d@sofit.pw>
2023-12-24 16:08:02 +0300

    feature add all courses

--- bot/handlers/courses.py
+++ bot/handlers/courses.py
@@ -1,21 +1,199 @@
+from json import loads
 from aiogram import Router, F
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import Message, KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.filters import Command, CommandStart, StateFilter
 
-from bot.serviceObjects import UserMessengerMng
-from bot.common import MessengerTypes
-from bot.handlers.utils import addBaseCommands
+from bot.serviceObjects import ProductMng, CourseMng
+from bot.engine import cache
+
+from bot.handlers.utils import updateUserCache, addBaseCommands
 
 router = Router()
 
 
+class AuthState(StatesGroup):
+    waitingProduct = State()
+    waitingAction = State()
+    waitingPlace = State()
+    waitingDetails = State()
+
+
+async def getProducts(message: Message, state: FSMContext) -> Message:
+    await state.clear()
+    products = await ProductMng().getList()
+    builder = ReplyKeyboardBuilder()
+    for i in range(len(products)):
+        products[i].order = i + 1
+        product_json = products[i].model_dump_json()
+        await cache.set(f'{products[i].order}.', product_json)
+        builder.add(KeyboardButton(text=f'{products[i].order}. {products[i].name}'))
+
+    builder.add(KeyboardButton(text='Повернутися назад'))
+    builder.adjust(2)
+    await state.set_state(AuthState.waitingProduct)
+    reply_markup = builder.as_markup(resize_keyboard=True)
+    return await message.answer(text='Обери курс', reply_markup=reply_markup)
+
+
+async def setProduct(message: Message, state: FSMContext, text: str) -> Message:
+    builder = ReplyKeyboardBuilder()
+    builder.add(KeyboardButton(text='Опис курсу'))
+    builder.add(KeyboardButton(text='Програма курсу'))
+    builder.add(KeyboardButton(text='Як проходить навчання'))
+    builder.add(KeyboardButton(text='Сертифікат'))
+    builder.add(KeyboardButton(text='Повернутись до вибору курсів'))
+    builder.adjust(2)
+
+    await state.set_state(AuthState.waitingAction)
+    reply_markup = builder.as_markup(resize_keyboard=True)
+    return await message.answer(text=text, reply_markup=reply_markup)
+
+
+async def getCourses(message: Message, state: FSMContext, userCache: str) -> Message:
+    product = loads(userCache).get('product')
+    order = product.get('order')
+    courses = await CourseMng().getList(**{'data': {'productUid': product.get('uid')}})
+    builder = ReplyKeyboardBuilder()
+    for i in range(len(courses)):
+        builder.add(KeyboardButton(text=f'{order}.{i + 1} {courses[i].name}'))
+        course_json = courses[i].model_dump_json()
+        await cache.set(f'{order}.{i + 1}', course_json)
+
+    builder.add(KeyboardButton(text='Повернутись до курсу'))
+    builder.adjust(2)
+    await state.set_state(AuthState.waitingPlace)
+    return await message.answer(text='Вибери місто', reply_markup=builder.as_markup(resize_keyboard=True))
+
+
+async def setDetail(message: Message, state: FSMContext, course: dict) -> Message:
+    employees = course.get('employees')
+    textEmployees = '<b><u>Викладачі</u></b>\n'
+    for employee in employees:
+        fullName = employee.get('fullName')
+        textEmployees += f'{fullName}\n'
+
+    location = course.get('location')
+    textLocation = f'<b><u>Адреса</u></b>\n{location}\n'
+
+    price = course.get('price')
+    textPrice = f'Вартість, грн: {price}\n'
+
+    builder = ReplyKeyboardBuilder()
+    builder.add(KeyboardButton(text='Основне'))
+    builder.add(KeyboardButton(text='Навчальний процес'))
+    builder.add(KeyboardButton(text='Iспит та акредитація'))
+    builder.add(KeyboardButton(text='Повернутись до вибору місто'))
+    builder.adjust(2)
+
+    await state.set_state(AuthState.waitingDetails)
+    reply_markup = builder.as_markup(resize_keyboard=True)
+
+    return await message.answer(text=f'{textEmployees}\n{textLocation}\n{textPrice}', reply_markup=reply_markup)
+
+
 @router.message(F.text == 'Базові курси навчання')
-async def cmdCourses(message: Message) -> None:
-    kbButton1 = [KeyboardButton(text='Інструктор групових програм EQF-3'),
-                 KeyboardButton(text='Інструктор тренажерного залу EQF-3')]
-    kbButton2 = [KeyboardButton(text='Персональний тренер EQF-4'),
-                 KeyboardButton(text='Дієтолог-нутріціолог')]
-    reply_markup = ReplyKeyboardMarkup(keyboard=[kbButton1, kbButton2], resize_keyboard=True, one_time_keyboard=True)
-    await message.answer(text='Представьтесь, отправьте контакт', reply_markup=reply_markup)
+async def cmdProducts(message: Message, state: FSMContext) -> None:
+    await getProducts(message=message, state=state)
+
+
+@router.message(AuthState.waitingProduct, F.text == 'Повернутися назад')
+async def cmdSetProduct(message: Message, state: FSMContext) -> None:
+    await state.clear()
+    await message.answer(text='Вибери дію', reply_markup=await addBaseCommands())
+
+
+@router.message(AuthState.waitingProduct)
+async def cmdSetProduct(message: Message, state: FSMContext) -> None:
+    if len(message.text) > 2:
+        value = await cache.get(message.text[:2])
+        if value is not None:
+            product = loads(value)
+            await updateUserCache(user=message.from_user.id, value={'product': product})
+            text = product.get('descriptions')
+            await setProduct(message=message, state=state, text=text)
+
+
+@router.message(AuthState.waitingAction, F.text == 'Повернутись до вибору курсів')
+async def cmdAction(message: Message, state: FSMContext) -> None:
+    await getProducts(message=message, state=state)
+
+
+@router.message(AuthState.waitingPlace, F.text == 'Повернутись до курсу')
+async def cmdDetails(message: Message, state: FSMContext) -> None:
+    await setProduct(message=message, state=state, text='Вибери дію')
+
+
+@router.message(AuthState.waitingDetails, F.text == 'Повернутись до вибору місто')
+async def cmdDetails(message: Message, state: FSMContext) -> None:
+    value = await cache.get(message.from_user.id)
+    if value is not None:
+        await getCourses(message=message, state=state, userCache=value)
+
+
+@router.message(AuthState.waitingAction, F.text == 'Опис курсу')
+async def cmdAction(message: Message) -> None:
+    value = await cache.get(message.from_user.id)
+    if value is not None:
+        product = loads(value).get('product')
+        await message.answer(text=product.get('descriptions'))
+
+
+@router.message(AuthState.waitingAction, F.text == 'Програма курсу')
+async def cmdAction(message: Message) -> None:
+    value = await cache.get(message.from_user.id)
+    if value is not None:
+        product = loads(value).get('product')
+        await message.answer(text=product.get('content'))
+
+
+@router.message(AuthState.waitingAction, F.text == 'Сертифікат')
+async def cmdAction(message: Message) -> None:
+    value = await cache.get(message.from_user.id)
+    if value is not None:
+        product = loads(value).get('product')
+        await message.answer(text=product.get('certificate'))
+
+
+@router.message(AuthState.waitingAction, F.text == 'Як проходить навчання')
+async def cmdAction(message: Message, state: FSMContext) -> None:
+    value = await cache.get(message.from_user.id)
+    if value is not None:
+        await getCourses(message=message, state=state, userCache=value)
+
+
+@router.message(AuthState.waitingPlace)
+async def cmdSetDetail(message: Message, state: FSMContext) -> None:
+    if len(message.text) > 3:
+        value = await cache.get(message.text[:3])
+        if value is not None:
+            course = loads(value)
+            await updateUserCache(user=message.from_user.id, value={'course': course})
+            await setDetail(message=message, state=state, course=course)
+
+
+@router.message(AuthState.waitingDetails, F.text == 'Основне')
+async def cmdAction(message: Message, state: FSMContext) -> None:
+    value = await cache.get(message.from_user.id)
+    if value is not None:
+        course = loads(value).get('course')
+        await setDetail(message=message, state=state, course=course)
+
+
+@router.message(AuthState.waitingDetails, F.text == 'Навчальний процес')
+async def cmdAction(message: Message) -> None:
+    value = await cache.get(message.from_user.id)
+    if value is not None:
+        course = loads(value).get('course')
+        section = course.get('descriptions')
+        await message.answer(text=f'<b><u>Навчальний процес</u></b>\n{section}')
+
+
+@router.message(AuthState.waitingDetails, F.text == 'Iспит та акредитація')
+async def cmdAction(message: Message) -> None:
+    value = await cache.get(message.from_user.id)
+    if value is not None:
+        course = loads(value).get('course')
+        section = course.get('exam')
+        await message.answer(text=f'<b><u>Iспит та акредитація</u></b>\n{section}')
