2c56f1e68a9cb55c145391fa938c8a449429d9ac
dave <d@sofit.pw>
2023-12-24 16:08:24 +0300

    chore(bot) add ReplyKeyboardBuilder

--- bot/handlers/utils.py
+++ bot/handlers/utils.py
@@ -1,12 +1,27 @@
+from json import loads, dumps
+
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+from bot.engine import cache
 
 
-async def addBaseCommands():
-    kbButton1 = [KeyboardButton(text='Базові курси навчання'), KeyboardButton(text='Семінари та майстер-класи')]
-    kbButton2 = [KeyboardButton(text='Мои данные')]
-    return ReplyKeyboardMarkup(keyboard=[kbButton1, kbButton2], resize_keyboard=True, one_time_keyboard=True)
+async def addBaseCommands() -> ReplyKeyboardMarkup:
+    builder = ReplyKeyboardBuilder()
+    builder.add(KeyboardButton(text='Базові курси навчання'))
+    builder.add(KeyboardButton(text='Семінари та майстер-класи'))
+    builder.add(KeyboardButton(text='Мої дані'))
+    builder.adjust(2)
+
+    return builder.as_markup(resize_keyboard=True)
 
 
 async def addCancelCommand():
-    kbButton = [KeyboardButton(text='Вернуться в главное меню')]
+    kbButton = [KeyboardButton(text='Повернутися до головного меню')]
     return ReplyKeyboardMarkup(keyboard=[kbButton], resize_keyboard=True, one_time_keyboard=True)
+
+
+async def updateUserCache(user, value: dict) -> None:
+    userCacheDict = loads(await cache.get(user))
+    userCacheDict.update(value)
+    await cache.set(user, dumps(userCacheDict))
