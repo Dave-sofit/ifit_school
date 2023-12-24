f09fe649ebe173a20465d3bdd9b24c31331e0f7f
dave <d@sofit.pw>
2023-12-24 16:07:39 +0300

    refactor (bot) del auth at startup

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -17,19 +17,20 @@ class AuthState(StatesGroup):
 
 @router.message(CommandStart())
 async def cmdStart(message: Message, state: FSMContext) -> None:
-    # Если поступила данная команда, то сбрасываем предыдущее состояние
-    await state.clear()
-
-    # Проверяем авторизованн ли данный пользователь
-    userMessenger = await UserMessengerMng().get(userMessengerId=message.from_user.id, noneable=True)
-    if userMessenger is None:
-        await state.set_state(AuthState.waitingContact)
-        kbButton = [KeyboardButton(text='Отправить номер телефона', request_contact=True)]
-        reply_markup = ReplyKeyboardMarkup(keyboard=[kbButton], resize_keyboard=True, one_time_keyboard=True)
-        await message.answer(text='Представьтесь, отправьте контакт', reply_markup=reply_markup)
-    else:
-        # await message.answer(text='начнем!')
-        await message.answer(text=f'{message.from_user.first_name} начнем!', reply_markup=await addBaseCommands())
+    # # Если поступила данная команда, то сбрасываем предыдущее состояние
+    # await state.clear()
+    #
+    # # Проверяем авторизованн ли данный пользователь
+    # userMessenger = await UserMessengerMng().get(userMessengerId=message.from_user.id, noneable=True)
+    # if userMessenger is None:
+    #     await state.set_state(AuthState.waitingContact)
+    #     kbButton = [KeyboardButton(text='Отправить номер телефона', request_contact=True)]
+    #     reply_markup = ReplyKeyboardMarkup(keyboard=[kbButton], resize_keyboard=True, one_time_keyboard=True)
+    #     await message.answer(text='Представьтесь, отправьте контакт', reply_markup=reply_markup)
+    # else:
+    #     # await message.answer(text='начнем!')
+    #     await message.answer(text=f'{message.from_user.first_name} начнем!', reply_markup=await addBaseCommands())
+    await message.answer(text=f'{message.from_user.first_name} начнем!', reply_markup=await addBaseCommands())
 
 
 @router.message(StateFilter(None), Command("cancel"))
@@ -42,15 +43,17 @@ async def cmdCancel(message: Message, state: FSMContext) -> None:
 async def cmdSetContact(message: Message, state: FSMContext) -> None:
     contact = message.contact
     if contact is not None and message.from_user.id == contact.user_id:
-        phone = contact.phone_number.replace('+', '').replace('-', '').replace('(', '').replace(')', '').replace(' ', '')
+        phone = contact.phone_number.replace('+', '').replace('-', '').replace('(', '').replace(')', '').replace(' ',
+                                                                                                                 '')
         user = await UserMng().first(phone=phone)
         if user is None:
             user = await UserMng().create(UserIn(firstName=contact.first_name, phone=phone))
 
         userMessenger = await UserMessengerMng().get(userMessengerId=contact.user_id, noneable=True)
         if userMessenger is None:
-            await UserMessengerMng().create(UserMessengerIn(userMessengerId=contact.user_id, type=MessengerTypes.telegram,
-                                                            userUid=user.uid))
+            await UserMessengerMng().create(
+                UserMessengerIn(userMessengerId=contact.user_id, type=MessengerTypes.telegram,
+                                userUid=user.uid))
 
         await message.answer(text=f'{message.from_user.first_name} номер получен, спасибо',
                              reply_markup=(await addBaseCommands()))
@@ -58,17 +61,3 @@ async def cmdSetContact(message: Message, state: FSMContext) -> None:
     else:
         await message.answer(text=f'{message.from_user.first_name} это не ваш номер')
         await state.clear()
-
-# Просто функция, которая доступна только администратору,
-# чей ID указан в файле конфигурации.
-# async def secretCommand(message: Message):
-#     await message.answer('Поздравляю! Эта команда доступна только администратору бота.')
-
-
-# def register_handlers_common(dp: Dispatcher, admin_id: int):
-#     dp.register_message_handler(cmdStart, commands='start', state="*")
-#     dp.register_message_handler(cmdCancel, commands='cancel', state="*")
-#     dp.register_message_handler(cmdCancel, Text(equals='отмена', ignore_case=True), state='*')
-#     dp.register_message_handler(cmdSetContact, content_types=['contact'], state="*")
-#     dp.register_message_handler(secretCommand, IDFilter(user_id=admin_id), commands='abracadabra')
-#     dp.register_message_handler(cmdCancel, Text(equals='Вернуться в главное меню', ignore_case=True), state='*')
