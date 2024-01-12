4a957509ccfddc1b46bb26b4bebe85deed669196
dave <d@sofit.pw>
2024-01-12 21:50:40 +0300

    refactor (models) add createdOn

--- bot/models/settlementsInfoRg.py
+++ bot/models/settlementsInfoRg.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, ForeignKey, Integer
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, TIMESTAMP
 from sqlalchemy_utils import UUIDType
 
 from bot.models.utils import InfoRgTable
@@ -6,9 +8,10 @@ from bot.models.utils import InfoRgTable
 
 class Settlement(InfoRgTable):
     __tablename__ = 'settlementsInfoRg'
+    createdOn = Column(TIMESTAMP(), default=datetime.utcnow, primary_key=True, nullable=False)
     courseUid = Column(UUIDType(), ForeignKey('coursesList.uid'), primary_key=True, nullable=False)
     userUid = Column(UUIDType(), ForeignKey('usersList.uid'), primary_key=True, nullable=False)
     amount = Column(Integer, nullable=False, default=0)
 
     def __repr__(self):
-        return f'{self.courseUid} - {self.userUid} - {self.amount}'
+        return f'{self.createdOn} - {self.courseUid} - {self.userUid} - {self.amount}'
